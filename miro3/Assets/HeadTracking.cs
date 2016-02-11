48d38dea02ea1ab5b87cc3c8bee1068b22e32ac1
KMU_VCL_YH <KMU_VCL_YH>
2016-02-11 16:35:08 +0900

    Folder updated

---
+++ miro3/Assets/HeadTracking.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Activate head tracking using the gyroscope
+public class HeadTracking : MonoBehaviour
+{
+   // public GameObject player; // First Person Controller parent node
+   // public GameObject head; // First Person Controller camera
+
+    // The initials orientation
+    private float initialOrientationX;
+    private float initialOrientationY;
+    private float initialOrientationZ;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Activate the gyroscope
+        Input.gyro.enabled = true;
+
+        // Save the firsts values
+        initialOrientationX = Input.gyro.rotationRateUnbiased.x;
+        initialOrientationY = Input.gyro.rotationRateUnbiased.y;
+        initialOrientationZ = -Input.gyro.rotationRateUnbiased.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the player and head using the gyroscope rotation rate
+        //player.transform.Rotate(0, initialOrientationY - Input.gyro.rotationRateUnbiased.y, 0);
+        //head.transform.Rotate(0, initialOrientationY - Input.gyro.rotationRateUnbiased.y,0);
+        gameObject.transform.Rotate(0, initialOrientationY - Input.gyro.rotationRateUnbiased.y, 0);
+        //게임 오브젝트 전체가 회전해야 회전축을 하나로 설정되어 fowrod가 제대로 적용됨
+        
+    }
+}
---
+++ miro3/Assets/moving.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class moving : MonoBehaviour
+{
+    public float speed = 5.0f;        // moving speed
+    private bool hasChanged =true;
+    private int counter=0;
+    private int guiCounter = 0;
+    float x_old = 0;
+    float y_old = 0;
+    float z_old = 0;
+
+    void movingforward(float amtToMove,int checkCounter) //moving smoothing
+    {
+        transform.Translate(Vector3.forward * checkCounter * 5 * amtToMove);
+    }
+    void Update()
+    {
+        float x = Input.acceleration.x;
+        float y = Input.acceleration.y;
+        float z = Input.acceleration.z;
+
+        float oldValue=(x_old*x)+(y_old*y)+(z_old*z);
+        float oldValueSqrt = Mathf.Abs(Mathf.Sqrt(oldValue));
+        float newValue = Mathf.Abs(Mathf.Sqrt((x * x) + (y * y) + (z * z)));
+
+        oldValue = oldValueSqrt * newValue;
+
+        if ((oldValue <= 0.8) && (oldValue > 0.7)) //민감도 조절
+        {
+            if (!hasChanged)
+            {
+                hasChanged = true;
+                counter++;
+                guiCounter++;
+            }
+            else
+                hasChanged = false;
+        }
+        x_old = x;
+        y_old = y;
+        z_old = z;
+
+        float amtToMove = speed * Time.deltaTime;  // moving distance per frame
+        movingforward(amtToMove,counter);
+        // 진동을 감지하면 그 순간에 움직임.
+        counter = 0;
+    }
+    void OnGUI()
+    {
+        GUILayout.TextArea(" counter  : " + guiCounter);
+        // 어느정도의 민감도 체크
+      
+    }
+
+}
